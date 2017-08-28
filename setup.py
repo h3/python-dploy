968e555839d7c2de5cc770b7ec86f0d7beba7442
Maxime Haineault <haineault@gmail.com>
2017-08-28 14:31:18 -0400

    Fixes

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='dploy',
+    name='fabric-contrib-dploy',
     version='0.0.1',
     description='Deployment utilities for fabric',
     long_description=(read('README.rst')),
@@ -17,9 +17,8 @@ setup(
     author_email='haineault@gmail.com',
     license='MIT',
     url='https://github.com/h3/fabric-contrib-dploy',
-    packages=find_packages(),
+    packages=['fabric.contrib.dploy'],
     include_package_data=True,
-    namespace_packages=['fabric'],
     zip_safe=True,
     classifiers=[
         'Environment :: Console',
